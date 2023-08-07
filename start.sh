57af3e668a2e225164f6af3c1c20d1bf8b173db6
deepaksharma1792001 <141623188+deepaksharma1792001@users.noreply.github.com>
2023-08-07 14:58:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,23 @@
-python3 update.py && python3 -m bot
+
+FROM anasty17/mltb:latest
+
+# Set the working directory to /usr/src/app
+WORKDIR /usr/src/app
+
+# Provide read, write, and execute permissions to the working directory
+RUN chmod 777 /usr/src/app
+
+# Copy the requirements.txt file to the working directory
+COPY requirements.txt .
+
+# Install the Python dependencies from requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+# Expose port 80
+EXPOSE 80
+
+# Copy all files from the current directory to the working directory
+COPY . .
+
+# Use the start.sh script as the command to run when the container starts
+CMD ["bash", "start.sh"]
